d4a99bfa873d4929c66c4381bb1054465bbe39a3
junior451 <kwabenaedwards@gmail.com>
2023-10-18 17:16:00 +0100

    add access modifiers to methods and attributes

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -2,43 +2,43 @@ import random
 
 class Hangman:
   def __init__(self, word_list, num_lives=5):
-    self.word = random.choice(word_list)
-    self.word_guessed = ['_'] * len(self.word)
-    self.num_letters = self.word_guessed.count("_")
+    self.__word = random.choice(word_list)
+    self.__word_guessed = ['_'] * len(self.__word)
+    self.num_letters = self.__word_guessed.count("_")
     self.num_lives = num_lives
-    self.word_list = word_list
-    self.list_of_guesses = []
+    self.__word_list = word_list
+    self.__list_of_guesses = []
 
-  def check_guess(self, guess):
+  def __check_guess(self, guess):
     guess = guess.lower()
 
-    if guess in self.word:
-      print("Good guess! {guess} is in the word.")
+    if guess in self.__word:
+      print(f"Good guess! {guess} is in the word.")
 
-      for index, letter in enumerate(self.word):
-        if letter == guess and self.word_guessed[index] == "_":
-          self.word_guessed[index] = letter
+      for index, letter in enumerate(self.__word):
+        if letter == guess and self.__word_guessed[index] == "_":
+          self.__word_guessed[index] = letter
         else:
           next  
 
-      self.num_letters = self.word_guessed.count("_")
+      self.num_letters = self.__word_guessed.count("_")
     else:
       self.num_lives -= 1
       print(f"Sorry, {guess} is not in the word. Try again.")
       print(f"You have {self.num_lives} lives left.")
 
-    print(self.word_guessed)
+    print(self.__word_guessed)
 
   def ask_for_input(self):
     guessed_letter = input("Enter a letter to guess")
 
     if len(guessed_letter) != 1 and guessed_letter.isalpha() == False:
       print("Invalid letter. Please, enter a single alphabetical character")
-    elif guessed_letter in self.list_of_guesses:
+    elif guessed_letter in self.__list_of_guesses:
       print(f"You already tried {guessed_letter}")
     else:
-      self.check_guess(guessed_letter)
-      self.list_of_guesses.append(guessed_letter)
+      self.__check_guess(guessed_letter)
+      self.__list_of_guesses.append(guessed_letter)
 
 def play_game(word_list):
   num_lives = 5
